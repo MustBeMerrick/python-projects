5be74d0f4df6a8cd085653be4ae439b963adbea4
Rosalie Jane <mshepherd000@gmail.com>
2024-01-31 13:52:15 -0800

    First Question Complete

---
+++ projects/ch1/1.py
@@ -0,0 +1,8 @@
+# 1) This program prompts a user for their name,
+#    then tells them to go blow a balloon.
+
+#Setting up a variable that the user will specify
+user=input('Please enter name:')
+#Setting up output based on user input
+print('Hey there,', user)
+print('Go blow a balloon...bitch')
