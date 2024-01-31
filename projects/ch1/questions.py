fcb97641bb19a7b1db3d9140ce42166edc46a7fa
Marc Feldman <marcryanfeldman@me.com>
2024-01-30 20:32:03 -0800

    added ch1 questions

---
+++ projects/ch1/questions.py
@@ -0,0 +1,24 @@
+# 1) Write a program that prompts a user for their name,
+#    then tells them to go blow a balloon
+
+# 2) Write a program that prompts a user for their favorite
+#    programming language, and if it's python, print out
+#    "you're a nerd". Otherwise print out "cool story bro"
+
+# 3) Write a program that takes two user inputs, score1 and
+#    score2, and prints out the higher score
+
+# 4) Write a program that takes in a user input, name, and
+#    says hello to them. Display an error if the input is
+#    a number
+
+# 5) Write a program that prints out the following menu
+#    and answers the question chosen (after prompting for user
+#    input)
+#    1: Print a smiley emoticon
+#    2: Multiply a number by 3
+#    3: Have python say hi to you
+  
+# 6) Extra Credit: Write a program that prompts the user for an
+#    integer and prints out that many values of the Fibonnaci sequence.
+#    The max value of the integer should be 7
