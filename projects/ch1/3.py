01efc19118ef26a5bdbb32db92060f61d2fdc0df
Rosalie Jane <mshepherd000@gmail.com>
2024-01-31 14:54:01 -0800

    Third Question Complete

---
+++ projects/ch1/3.py
@@ -0,0 +1,12 @@
+# 3) Write a program that takes two user inputs, score1 and
+#    score2, and prints out the higher score
+
+#Create two variables based on user input
+Score1=input('Enter Score1:')
+Score2=input('Enter Score2:')
+#Display the larger number
+if int(Score1)>int(Score2):
+  print('Highscore: ',Score1)
+
+if int(Score1)<int(Score2):
+  print('Highscore: ',Score2)
