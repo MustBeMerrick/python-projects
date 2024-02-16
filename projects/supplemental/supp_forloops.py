92fa8803736a3bd4cf782435b00f2030371ee53f
Rosalie Jane <mshepherd000@gmail.com>
2024-02-15 16:21:21 -0800

    for loops

---
+++ projects/supplemental/supp_forloops.py
@@ -0,0 +1,58 @@
+#1)  Write code that prints out the numbers 1 through 10
+
+  #for item in range(1,11):
+  #  print(item)
+
+#2)  Write code that prints out the numbers 5 through 49
+
+  #for item in range(5,50):
+  #  print(item)
+
+#3)  Write code that prompts the user for a number and prints out the numbers 0 through their number
+
+  #user_num=int(input('Enter a number: '))
+  #
+  #for i in range(user_num+1):
+  #  print(i)
+
+#4)  Write code that prints out all even numbers between 1 and 100
+
+  #for i in range(1,101):
+  #  if i % 2 == 0:
+  #    print(i)
+
+#5)  Write code that prints out all odd numbers between 1 and 100
+
+  #for i in range(1,101):
+  #  if i % 2 != 0:
+  #    print(i)
+
+#6)  Write code that prints out all numbers between 1 and 100 that are multiples of 3
+
+  #for i in range(1,101):
+  #  if i <=33:
+  #    print(i*3)
+
+  #for i in range(1,101):
+  #  if i%3==0:
+  #    print(i)
+
+#7)  Write code that prompts user for a string and print out each character of that string
+
+  #user_input=input('Enter word: ')
+  #
+  #for i in user_input:
+  #  print(i)
+
+#8)  Write code that prompts user for 5 objects and use them to create a list. Then, print out each item in the list        
+
+  #user_list1=input('Enter word 1: ')
+  #user_list2=input('Enter word 2: ')
+  #user_list3=input('Enter word 3: ')
+  #user_list4=input('Enter word 4: ')
+  #user_list5=input('Enter word 5: ')
+  #
+  #list = [user_list1,user_list2,user_list3,user_list4,user_list5]
+  #
+  #for i in list:
+  #  print(i)
