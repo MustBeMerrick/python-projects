e9d8fdd7442374be3d4914b197f5fc1b13654cca
Rosalie Jane <mshepherd000@gmail.com>
2024-03-01 13:16:59 -0800

    while loops

---
+++ projects/supplemental/supp_whileloops.py
@@ -0,0 +1,80 @@
+#-------------- WHILE Loops --------------
+#1)  Write code that prints out the numbers 1 through 10
+#i=1
+#while 1<=i<=10:
+#  print(i)
+#  i+=1
+
+#2)  Write code that prints out the numbers 5 through 49
+#i=5
+#while 5<=i<=49:
+#  print(i)
+#  i+=1
+
+#3)  Write code that prompts the user for a number and prints out the numbers 0 through their number
+#user_input=int(input('Enter Number: '))
+#
+#i=user_input
+#h=0
+#while 0<=h<=i:
+#  print(h)
+#  h=h+1
+
+#4)  Write code that prints out all even numbers between 1 and 100
+
+#i=1
+#while 1<=i<=100:
+#  if i%2==0:
+#    print(i)
+#  i+=1
+
+#5)  Write code that prints out all odd numbers between 1 and 100
+
+#i=1
+#while 1<=i<=100:
+#  if i%2!=0:
+#    print(i)
+#  i+=1
+
+#6)  Write code that prints out all numbers between 1 and 100 that are multiples of 3
+
+#i=1
+#while 1<=i<=100:
+#  if i%3==0:
+#    print(i)
+#  i+=1
+
+#7)  Write code that prompts user for a string and print out each character of that string
+
+#user_input=input('Input word: ')
+#
+#user_length=len(user_input)
+#
+#i=0
+#
+#while i<user_length:
+#  print(user_input[i])
+#  i+=1
+
+
+#8)  Write code that prompts user for 5 objects and use them to create a list. Then, print out each item in #the list
+
+#user_list1=input('Enter word 1: ')
+#user_list2=input('Enter word 2: ')
+#user_list3=input('Enter word 3: ')
+#user_list4=input('Enter word 4: ')
+#user_list5=input('Enter word 5: ')
+#  
+#list = [user_list1,user_list2,user_list3,user_list4,user_list5]
+#
+#list_len=len(list)
+#
+#i=0
+#
+#while i<list_len:
+#  print(list[i])
+#  i+=1
+
+#** HINTS! **
+#  - Try initializing a separate variable (i=0, char_i=0, etc) to use as an iterator
+#  - If stuck, you can always simply use an infinite while loop (while True:) combined with an if/else to break out
