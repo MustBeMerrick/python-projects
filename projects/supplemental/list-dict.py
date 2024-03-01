7537355bd52a76bdf65f98c13ef04f1ee479f1ac
Rosalie Jane <mshepherd000@gmail.com>
2024-03-01 15:22:25 -0800

    list-dict

---
+++ projects/supplemental/list-dict.py
@@ -0,0 +1,82 @@
+#-------------- Lists/Dictionaries --------------
+#Create the following list: (apple, salad, chips, cake, yogurt, peppers, wine)
+
+#grocery_list=['apple', 'salad', 'chips', 'cake', 'yogurt', 'peppers', 'wine']
+
+#21) Print out the 3rd item in the list
+
+#print('3rd item: ',grocery_list[3])
+
+#22) Print out the 4th item in the list
+
+#print('4th item: ',grocery_list[4])
+
+#23) Print out the last item in the list
+
+#print('last item: ',grocery_list[-1])
+
+#24) Prompt the user for a number between [0-6] and print out that item in the list
+
+#user_number=int(input('Enter integer[0-6]: '))
+#
+#print('User pick: ',grocery_list[user_number])
+
+
+#25) Prompt the user for two numbers between [0-6] and print out those two items in the list
+
+#user_number1=int(input('Enter integer[0-6]: '))
+#user_number2=int(input('Enter integer[0-6]: '))
+#
+#print('User Pick 1: ', grocery_list[user_number1])
+#print('User Pick 2: ', grocery_list[user_number2])
+
+
+#26) Print out all items in the list
+
+#print(grocery_list)
+
+#
+#Create the following dictionary: 
+#apple:   1
+#salad:   3
+#chips:   2
+#cake:    9
+#yogurt:  2
+#peppers: 4
+#wine     12
+
+#grocery_dict={'apple':1, 'salad':3, 'chips':2, 'cake':9, 'yogurt':2, 'peppers':4, 'wine':12}
+
+#27) Print out the number associated with cake
+
+#print('cake:\t',grocery_dict['cake'])
+
+#28) Print out the number associated with apple
+
+#print('apple:\t',grocery_dict['apple'])
+
+#29) Print out the number associated with wine
+
+#print('wine:\t',grocery_dict['wine'])
+
+#30) Prompt user for an item in the list, and print out the number associated with that item
+
+#user_pick=input('Select item from list: ')
+
+#print(user_pick,':',grocery_dict[user_pick])
+
+#31) Print out the length of the list
+
+#print('Length of dict: ',len(grocery_dict))
+
+#32) Write code to append an item to the dictionary. Append "carrots: 5"
+
+#grocery_dict['carrots']=5
+
+#33) Write code to iterate ofer the dictionary and print out each number associated with each item
+
+#print(grocery_dict.values())
+
+
+
+
