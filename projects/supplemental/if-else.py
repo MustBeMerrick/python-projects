c509cf4ecfaabe893f8499457d3e85d7616c8b2b
Rosalie Jane <mshepherd000@gmail.com>
2024-03-01 13:48:11 -0800

    if-else

---
+++ projects/supplemental/if-else.py
@@ -0,0 +1,60 @@
+#17) Write code that prompts the user for their age. If the number is less than 4, print "You're a baby". 
+#Otherwise, print "You're not a baby"
+
+#user_age=int(input('What is your age? '))
+#
+#if user_age<4:
+#  print('You\'re a baby')
+#else:
+#  print('You\'re not a baby')
+
+
+#18) Write code that prompts the user for their age. 
+#    If < 4, print "You're a baby"
+#    If between 4 and 18, print "You're still growing"
+#    If > 18, print "You're an adult"
+
+#user_age=int(input('What is your age? '))
+#
+#if user_age<4:
+#  print('You\'re a baby')
+#elif user_age>18:
+#  print('You\'re an adult')  
+#else:
+#  print('You\'re still growing')
+
+
+#19) Write code that prompts the user for their age and height (in feet).
+#    If their age is < 12 print "You're still growing"
+#    Otherwise, if their height is > 6.5 ft, print "You're super tall"
+
+#user_age=int(input('What is your age? '))
+#user_height=float(input('What is your height (in ft.)? '))
+#
+#if user_age<12:
+#  print('You\'re still growing')
+#elif user_height>6.5:
+#  print('You\'re super tall')
+#else:
+#  print(':)')
+
+
+
+#20) Write code that prompts the user for their age and height (in feet).
+#    If their age is < 12 print "You're still growing"
+#    Otherwise, if their age is > 12 and their height is less than 4 ft, print "You might be a dwarf"
+#    Otherwise, if their age is > 12 and their height is greater than 6.5 ft, print "You're super tall"
+#    If none of the conditions are met, print "Guess you're average"
+
+#user_age=int(input('What is your age? '))
+#user_height=float(input('What is your height (in ft.)? '))
+#
+#if user_age<12:
+#  print('You\'re still growing')
+#elif user_age>12 and user_height<4:
+#  print('You might be a dwarf')
+#elif user_age >12 and user_height>6.5:
+#  print('You\'re super tall')
+#else:
+#  print('You are average-tastic')
+#  print(':)')
